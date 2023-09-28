c615fb00441497591dcc19c1a1d2f45446049202
verezeeee <theusma58@gmail.com>
2023-09-28 04:14:14 -0300

    connection to db fix

--- database/connect.js
+++ database/connect.js
@@ -1,10 +1,19 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const user = process.env.MONGO_USER;
+const password = process.env.MONGO_PASS;
+const database = process.env.MONGO_DBNAME;
+const uri = `mongodb+srv://${user}:${password}@${database}.mongodb.net/?retryWrites=true&w=majority`
 
 export default async function connect() {
-    const mongoServer = await MongoMemoryServer.create();
-    const mongoUri = mongoServer.getUri();
-
-    await mongoose.connect(mongoUri, { dbName: "PrecisionTech"});
-    console.log(`MongoDB successfully connected to ${mongoUri}`);
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('Conectado ao banco de dados');
+    }).catch((error) => {
+        console.log(error);
+    });
 }
